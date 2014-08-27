1769b87ce540f957b65db2c52f6e075ff979b803
Eric Angle <Eric@Macintosh-2.local>
2014-08-26 19:44:50 -0600

    all tests now pass

--- algebra.py
+++ algebra.py
@@ -8,7 +8,6 @@ def complex(a,b):
   return myComplex
 
 def quat(a,b,c,d):
-# check that a and b are complex numbers
   myQuat = list()
   myComplex1 = list()
   myComplex2 = list()
@@ -19,6 +18,30 @@ def quat(a,b,c,d):
   myQuat.append(myComplex1)
   myQuat.append(myComplex2)
   return myQuat
+
+def octon(a,b,c,d,e,f,g,h):
+  myOcton = list()
+  myQuat1 = list()
+  myQuat2 = list()
+  myComplex1 = list()
+  myComplex2 = list()
+  myComplex3 = list()
+  myComplex4 = list()
+  myComplex1.append(a)
+  myComplex1.append(b)
+  myComplex2.append(c)
+  myComplex2.append(d)
+  myComplex3.append(e)
+  myComplex3.append(f)
+  myComplex4.append(g)
+  myComplex4.append(h)
+  myQuat1.append(myComplex1)
+  myQuat1.append(myComplex2)
+  myQuat2.append(myComplex3)
+  myQuat2.append(myComplex4)
+  myOcton.append(myQuat1)
+  myOcton.append(myQuat2)
+  return myOcton
 
 # Calculates total length of nested list
 def listLen(myList):
@@ -47,10 +70,9 @@ def sub(list1,list2):
     return [sub(list1[i],list2[i]) for i in xrange(len(list1))]
 
 # Multiplication
-# (a,b)(c,d) = (ac-db*,a*d+bc)
+# (a,b)(c,d) = (ac-d*b,da+bc*)
 def mult(list1,list2):
-  length = listLen(list1)
-  if length == 1:
+  if listLen(list1) == 1:
     return list1*list2
   else:
     total = list()
@@ -58,31 +80,52 @@ def mult(list1,list2):
     b = list1[1]
     c = list2[0]
     d = list2[1]
-    left = sub(mult(a,c),mult(d,conj(b)))
-    right = add(mult(conj(a),d),mult(b,c))
+    left = sub(mult(a,c),mult(conj(d),b))
+    right = add(mult(d,a),mult(b,conj(c)))
     total.append(left)
     total.append(right)
     return total
 
+# Modulus
+# |a| = (a a*)^1/2
+def mod(myList):
+  if listLen(myList) == 2:
+    return pow(mult(myList,conj(myList))[0],0.5)
+  if listLen(myList) == 4:
+    return pow(mult(myList,conj(myList))[0][0],0.5)
+  if listLen(myList) == 8:
+    return pow(mult(myList,conj(myList))[0][0][0],0.5)
+
 # Division
 # a / b = ab* / (bb*)
 # def div(list1,list2)
+def div(list1,list2):
+  modSqList2 = pow(mod(list2),2.0)
+  invModSqList2 = list()
+  if listLen(list1) == 2:
+    invModSqList2 = complex(1.0/modSqList2,0.0)
+  if listLen(list1) == 4:
+    invModSqList2 = quat(1.0/modSqList2,0,0,0)
+  if listLen(list1) == 8:
+    invModSqList2 = octon(1.0/modSqList2,0.0)
+  return mult(mult(list1,conj(list2)),invModSqList2)
 
 # Conjugation
 # (a,b)* = (a*,-b)
 def conj(myList):
-  length = listLen(myList)
-  if length == 1:
+  if listLen(myList) == 1:
     return myList
   else:
     left = conj(myList[0])
     if listLen(left) == 1:
-      right = sub(0,myList[1])
-    else:
+      zeroes = 0
+    elif listLen(left) == 2:
       zeroes = list()
-      for i in xrange(listLen(left)):
-        zeroes.append(0)
-      right = sub(zeroes,myList[1])
+      zeroes = complex(0,0)
+    elif listLen(left) == 4:
+      zeroes = list()
+      zeroes = quat(0,0,0,0)
+    right = sub(zeroes,myList[1])
     result = list()
     result.append(left)
     result.append(right)
--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import algebra
 
+print " "
+
 ###########
 # Complex #
 ###########
 
 nTrueC = 0
-nTestsC = 4
+nTestsC = 6
 
 C1 = algebra.complex(1,2)
 C2 = algebra.complex(3,4)
@@ -19,12 +21,18 @@ nTrueC = nTrueC + (algebra.sub(C1,C2) == algebra.complex(1-3,2-4))
 # Test 3: Conjugate
 nTrueC = nTrueC + (algebra.conj(C1) == algebra.complex(1,-2))
 
-# Test 4: Multiplication
+# Test 4: Modulus
+nTrueC = nTrueC + (algebra.mod(C1) == pow(sum (pow(i,2.0) for i in range(1,2+1)),0.5))
+
+# Test 5: Multiplication
 nTrueC = nTrueC + (algebra.mult(C1,C2) == algebra.complex(1*3-2*4,1*4+2*3))
+
+# Test 6: Division
+nTrueC = nTrueC + (algebra.div(C1,C2) == algebra.complex(11.0/25.0,2.0/25.0))
 
 # Results
 if (nTrueC == nTestsC):
-  print "Complex tests pass"
+  print "Complex tests pass: " + str(nTrueC) + " passed out of " + str(nTestsC)
 else:
   print "Complex tests fail: " + str(nTrueC) + " passed out of " + str(nTestsC)
   
@@ -33,7 +41,7 @@ else:
 ##############
 
 nTrueQ = 0
-nTestsQ = 3 + 16
+nTestsQ = 4 + 4*4
 
 Q1 = algebra.quat(1,2,3,4)
 Q2 = algebra.quat(5,6,7,8)
@@ -47,7 +55,10 @@ nTrueQ = nTrueQ + (algebra.sub(Q1,Q2) == algebra.quat(1-5,2-6,3-7,4-8))
 # Test 3: Conjugate
 nTrueQ = nTrueQ + (algebra.conj(Q1) == algebra.quat(1,-2,-3,-4))
 
-# Test 4: Multiplication
+# Test 4: Modulus
+nTrueQ = nTrueQ + (algebra.mod(Q1) == pow(sum (pow(i,2.0) for i in range(1,4+1)),0.5))
+
+# Test 5: Multiplication
 E0  = algebra.quat(1,0,0,0)
 nE0 = algebra.quat(-1,0,0,0)
 E1  = algebra.quat(0,1,0,0)
@@ -59,28 +70,60 @@ nE3 = algebra.quat(0,0,0,-1)
 
 for i in xrange(4):
   for j in xrange(4):
-    q1 = algebra.quat(i==0,i==1,i==2,i==3)    
-    q2 = algebra.quat(j==0,j==1,j==2,j==3)
+    q1 = algebra.quat((i==0)*1,(i==1)*1,(i==2)*1,(i==3)*1)    
+    q2 = algebra.quat((j==0)*1,(j==1)*1,(j==2)*1,(j==3)*1)
     q3 = algebra.mult(q1,q2)
+
+    # 3 Tests: i,j = 1,2,3
     if (i == j and i != 0):
       nTrueQ = nTrueQ + (q3 == nE0)
+    # 4 Tests: i = 0, j = 0,1,2,3
     if (i == 0):
       nTrueQ = nTrueQ + (q3 == algebra.quat(j==0,j==1,j==2,j==3))
+    # 3 Tests: j = 0, i = 1,2,3
     elif (j == 0):
       nTrueQ = nTrueQ + (q3 == algebra.quat(i==0,i==1,i==2,i==3))
-    nTrueQ = nTrueQ + (i == 2 and j == 1)*(q3 == nE3)
-    + (i == 3 and j == 1)*(q3 == E2)
-    + (i == 3 and j == 2)*(q3 == nE1)
-    + (i == 1 and j == 2)*(q3 == E3)
-    + (i == 1 and j == 3)*(q3 == nE2)
-    + (i == 2 and j == 3)*(q3 == E1)
+    # 6 Tests: off diag with i and j not 0
+    nTrueQ = nTrueQ + (i == 2 and j == 1)*(q3 == nE3) + (i == 3 and j == 1)*(q3 == E2) + (i == 3 and j == 2)*(q3 == nE1) + (i == 1 and j == 2)*(q3 == E3) + (i == 1 and j == 3)*(q3 == nE2) + (i == 2 and j == 3)*(q3 == E1)
+
+# Test 6: Division
 
 # Results
 if (nTrueQ == nTestsQ):
-  print "Quaternion tests pass"
+  print "Quaternion tests pass: " + str(nTrueQ) + " passed out of " + str(nTestsQ)
 else:
   print "Quaternion tests fail: " + str(nTrueQ) + " passed out of " + str(nTestsQ)
 
 ############
 # Octonion #
 ############
+
+nTrueO = 0
+nTestsO = 4
+
+O1 = algebra.octon(1,2,3,4,5,6,7,8)
+O2 = algebra.octon(9,10,11,12,13,14,15,16)
+
+# Test 1: Addition
+nTrueO = nTrueO + (algebra.add(O1,O2) == algebra.octon(1+9,2+10,3+11,4+12,5+13,6+14,7+15,8+16))
+
+# Test 2: Subtraction
+nTrueO = nTrueO + (algebra.sub(O1,O2) == algebra.octon(1-9,2-10,3-11,4-12,5-13,6-14,7-15,8-16))
+
+# Test 3: Conjugate
+nTrueO = nTrueO + (algebra.conj(O1) == algebra.octon(1,-2,-3,-4,-5,-6,-7,-8))
+
+# Test 4: Modulus
+nTrueO = nTrueO + (algebra.mod(O1) == pow(sum (pow(i,2.0) for i in range(1,8+1)),0.5) )
+
+# Test 5: Multiplication
+
+# Test 6: Division
+
+# Results
+if (nTrueO == nTestsO):
+  print "Octonion tests pass: " + str(nTrueO) + " passed out of " + str(nTestsO)
+else:
+  print "Octonion tests fail: " + str(nTrueO) + " passed out of " + str(nTestsO)
+
+print " "
