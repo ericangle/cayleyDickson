f10d5de22267328f0234f4184788f6e34ce68a68
Eric Angle <eric.lohner.angle@gmail.com>
2015-09-01 18:59:56 -0600

    adding some rotation tests

--- test/testRotation.py
+++ test/testRotation.py
@@ -1,12 +1,20 @@
 from rotation import rotate as rot
 from math import pi
 
-n = [1.0,1.0,1.0]
-V = [1.0,0.0,0.0]
-T = 2.0*pi/3.0
+n1    = [1.0,1.0,1.0]
+V1    = [1.0,0.0,0.0]
+T1    = 2.0*pi/3.0
+V1rot = [0.0,1.0,0.0]
+
+n2    = [0.0,0.0,1.0]
+V2    = [0.0,1.0,0.0]
+T2    = pi/2.0
+V2rot = [-1.0,0.0,0.0]
 
 def test_rotate():
-  Vrotated_calc   = rot(V,2.0*pi/3.0,n)
-  Vrotated_accept = [0.0,1.0,0.0]
-  for i in range(len(Vrotated_calc)):
-    assert abs(Vrotated_calc[i] - Vrotated_accept[i]) < 1.0e-6
+  def compare(calc,accept):
+    for i in range(len(calc)):
+      assert abs(calc[i] - accept[i]) < 1.0e-6
+
+  compare(rot(V1,T1,n1),V1rot)
+  compare(rot(V2,T2,n2),V2rot)
