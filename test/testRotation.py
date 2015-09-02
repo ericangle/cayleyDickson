a44806d96fda1141b7614ff21785f1c79038758a
Eric Angle <eric.lohner.angle@gmail.com>
2015-09-02 07:11:11 -0600

    adding a rotation test

--- test/testRotation.py
+++ test/testRotation.py
@@ -1,5 +1,5 @@
 from rotation import rotate as rot
-from math import pi
+from math import pi,cos,sin
 
 n1    = [1.0,1.0,1.0]
 V1    = [1.0,0.0,0.0]
@@ -11,6 +11,11 @@ V2    = [0.0,1.0,0.0]
 T2    = pi/2.0
 V2rot = [-1.0,0.0,0.0]
 
+n3    = [1.0,1.0,0.0]
+V3    = [1.0,0.0,0.0]
+T3    = pi/2.0
+V3rot = [cos(pi/4.0)*cos(pi/4.0),cos(pi/4.0)*sin(pi/4.0),-sin(pi/4.0)]
+
 def test_rotate():
   def compare(calc,accept):
     for i in range(len(calc)):
@@ -18,3 +23,4 @@ def test_rotate():
 
   compare(rot(V1,T1,n1),V1rot)
   compare(rot(V2,T2,n2),V2rot)
+  compare(rot(V3,T3,n3),V3rot)
