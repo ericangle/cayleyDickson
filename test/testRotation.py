6ed5a745616c4c44f323faee3574d6416e869d2c
Eric Angle <eric.lohner.angle@gmail.com>
2015-09-29 21:18:40 -0600

    rotation now also computed using generators

--- test/testRotation.py
+++ test/testRotation.py
@@ -1,5 +1,6 @@
 from rotation import rotate as rot
 from math import pi,cos,sin
+import numpy as np
 
 n1    = [1.0,1.0,1.0]
 V1    = [1.0,0.0,0.0]
@@ -16,11 +17,30 @@ V3    = [1.0,0.0,0.0]
 T3    = pi/2.0
 V3rot = [cos(pi/4.0)*cos(pi/4.0),cos(pi/4.0)*sin(pi/4.0),-sin(pi/4.0)]
 
-def test_rotate():
-  def compare(calc,accept):
-    for i in range(len(calc)):
-      assert abs(calc[i] - accept[i]) < 1.0e-6
+# Generators of rotations
+Jx = np.array([[0,0,0],
+               [0,0,-1.0],
+               [0,1.0,0]])
+Jy = np.array([[0,0,1.0],
+               [0,0,0],
+               [-1.0,0,0]])
+Jz = np.array([[0,-1.0,0],
+               [1.0,0,0],
+               [0,0,0]])
 
-  compare(rot(V1,T1,n1),V1rot)
-  compare(rot(V2,T2,n2),V2rot)
-  compare(rot(V3,T3,n3),V3rot)
+iden = np.array([[1.0,0,0],
+                 [0,1.0,0],
+                 [0,0,1.0]])
+
+def test_rotate():
+  def useExp(V,T,n):
+    return np.dot(np.linalg.matrix_power(iden + T*(n[0]*Jx + n[1]*Jy + n[2]*Jz)/10000,10000),np.array([[V[0]],[V[1]],[V[2]]]))
+
+  def compare(calc,accept1,accept2):
+    for i in range(len(calc)):
+      assert abs(calc[i] - accept1[i]) < 1.0e-6
+      assert abs(calc[i] - accept2[i][0]) < 1.0e-3
+
+  compare(rot(V1,T1,n1),V1rot,useExp(V1,T1,n1))
+  compare(rot(V2,T2,n2),V2rot,useExp(V2,T2,n2))
+  compare(rot(V3,T3,n3),V3rot,useExp(V3,T3,n3))
